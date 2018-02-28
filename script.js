5890d329eaa0e3be78190f6bf0a548944c8b190e
Lukas Michaels <lukasmichaels@Lukas-MacPro.local>
2018-02-27 20:07:53 -0500

    Add full date functions

--- script.js
+++ script.js
@@ -30,3 +30,72 @@ function getESTHours() {
 
     return d;
 }
+
+function getCurrentDay() {
+    var d = new Date();
+	d = d.getDay();
+
+    if(d==0){
+    	d = "Sunday";
+    } else if(d==1){
+    	d = "Monday";
+    } else if(d==2){
+    	d = "Tuesday";
+    } else if(d==3){
+    	d = "Wednesday";
+    } else if(d==4){
+    	d = "Thursday";
+    } else if(d==5){
+    	d = "Friday";
+    } else if(d==5){
+    	d = "Saturday";
+    } 
+
+    return d;
+}
+
+function getCurrentMonth() {
+    var d = new Date();
+	d = d.getMonth();
+
+    if(d==0){
+    	d = "January";
+    } else if(d==1){
+    	d = "February";
+    } else if(d==2){
+    	d = "March";
+    } else if(d==3){
+    	d = "April";
+    } else if(d==4){
+    	d = "May";
+    } else if(d==5){
+    	d = "June";
+    } else if(d==6){
+    	d = "July";
+    } else if(d==7){
+    	d = "August";
+    } else if(d==8){
+    	d = "September";
+    } else if(d==9){
+    	d = "October";
+    } else if(d==10){
+    	d = "November";
+    } else if(d==11){
+    	d = "December";
+    }
+
+    return d;
+}
+
+function getCurrentYear() {
+    var d = new Date();
+	d = d.getFullYear();
+	return d;
+}
+
+function getFullDate() {
+	var date = new Date();
+    var date = getCurrentDay() + " " + getCurrentMonth() + " " + date.getDate() + ", " + getCurrentYear()
+
+    return date;
+}
